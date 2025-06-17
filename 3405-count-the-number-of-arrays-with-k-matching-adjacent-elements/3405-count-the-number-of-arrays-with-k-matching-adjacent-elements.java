267b43f4b2d055975e3709590d4ae049510a3004
Naveena-kemburu <153837261+Naveena-kemburu@users.noreply.github.com>
2025-06-17 22:44:06 +0530

    Time: 69 ms (51.22%), Space: 42.5 MB (53.66%) - LeetHub

--- 3405-count-the-number-of-arrays-with-k-matching-adjacent-elements/3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.java
+++ 3405-count-the-number-of-arrays-with-k-matching-adjacent-elements/3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.java
@@ -1,19 +1,34 @@
 class Solution {
-    private static final int MOD = 1_000_000_007;
+    private static final int MOD = (int)1e9 + 7;
+    private static final int N = (int)1e5 + 10;
+    private static final long[] fact = new long[N], invFact = new long[N];
+
+    static {
+        fact[0] = invFact[0] = 1;
+        for (int i = 1; i < N; ++i) {
+            fact[i] = fact[i - 1] * i % MOD;
+            invFact[i] = modPow(fact[i], MOD - 2);
+        }
+    }
+
+    private static long modPow(long a, int e) {
+        long res = 1;
+        while (e > 0) {
+            if ((e & 1) == 1) res = res * a % MOD;
+            a = a * a % MOD;
+            e >>= 1;
+        }
+        return res;
+    }
+
+    private static long nCk(int n, int k) {
+        return fact[n] * invFact[k] % MOD * invFact[n - k] % MOD;
+    }
 
     public int countGoodArrays(int n, int m, int k) {
-        long[] dp = new long[k + 2];
-        dp[0] = m;
-
-        for (int i = 2; i <= n; i++) {
-            long[] next = new long[k + 2];
-            for (int j = 0; j <= Math.min(i - 2, k); j++) {
-                next[j] = (next[j] + dp[j] * (m - 1)) % MOD;
-                next[j + 1] = (next[j + 1] + dp[j]) % MOD;
-            }
-            dp = next;
-        }
-
-        return (int) dp[k];
+        if (k > n - 1) return 0;
+        long combinations = nCk(n - 1, k);
+        long ways = combinations * m % MOD * modPow(m - 1, n - k - 1) % MOD;
+        return (int) ways;
     }
 }
