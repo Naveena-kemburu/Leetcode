0172c1c89fd78f5761e5ca063cc927b9605f0535
Naveena-kemburu <153837261+Naveena-kemburu@users.noreply.github.com>
2025-06-27 18:59:14 +0530

    Time: 197 ms (92.45%), Space: 45.6 MB (18.87%) - LeetHub

--- 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.java
+++ 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.java
@@ -3,42 +3,43 @@ class Solution {
         int[] freq = new int[26];
         for (char c : s.toCharArray()) freq[c - 'a']++;
 
+        // Reduce search space to only characters appearing at least k times
         List<Character> valid = new ArrayList<>();
-        for (int i = 25; i >= 0; i--) {
+        for (int i = 0; i < 26; i++) {
             if (freq[i] >= k) {
-                int times = freq[i] / k;
-                for (int j = 0; j < times; j++) {
-                    valid.add((char) ('a' + i));
+                valid.add((char)(i + 'a'));
+            }
+        }
+
+        // BFS: Try all sequences up to length 7
+        Queue<String> queue = new LinkedList<>();
+        queue.add("");
+        String res = "";
+
+        while (!queue.isEmpty()) {
+            String curr = queue.poll();
+            for (char c : valid) {
+                String next = curr + c;
+                if (isKSubsequence(s, next, k)) {
+                    queue.add(next);
+                    // Update answer if longer or lexicographically larger
+                    if (next.length() > res.length() || 
+                       (next.length() == res.length() && next.compareTo(res) > 0)) {
+                        res = next;
+                    }
                 }
             }
         }
 
-        res = "";
-        dfs("", valid, s, k);
         return res;
     }
 
-    String res;
-
-    private void dfs(String curr, List<Character> valid, String s, int k) {
-        if (curr.length() > 7) return;
-        if (curr.length() > res.length() || (curr.length() == res.length() && curr.compareTo(res) > 0)) {
-            if (isKSubsequence(s, curr, k)) {
-                res = curr;
-            }
-        }
-
-        for (char c : valid) {
-            dfs(curr + c, valid, s, k);
-        }
-    }
-
-    private boolean isKSubsequence(String s, String seq, int k) {
+    private boolean isKSubsequence(String s, String target, int k) {
         int i = 0, count = 0;
         for (char c : s.toCharArray()) {
-            if (c == seq.charAt(i)) {
+            if (c == target.charAt(i)) {
                 i++;
-                if (i == seq.length()) {
+                if (i == target.length()) {
                     count++;
                     if (count == k) return true;
                     i = 0;
@@ -48,4 +49,3 @@ class Solution {
         return false;
     }
 }
-
